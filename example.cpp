ea516cd6e64d250896b1d76d1a50bbfb1f12e3b7
trodevel <trodevel@gmail.com>
2020-05-15 02:44:49 +0200

    extended example

--- example.cpp
+++ example.cpp
@@ -2,8 +2,27 @@
 
 #include "parser.h"
 #include "csv_helper.h"     // CsvHelper::to_csv()
+#include "validator.h"
+
+template <class T>
+void validate( const T & o, const std::string & name )
+{
+    try
+    {
+        basic_objects::validator::validate( name, o );
+        std::cout << name << " : valid" << std::endl;
+    }
+    catch( std::exception & e )
+    {
+        std::cout << name << " : invalid : " << e.what() << std::endl;
+    }
+}
 
 int main()
 {
+    basic_objects::Email o;
+
+    validate( o, "EMAIL" );
+
     return 0;
 }
