0159c5bfccb88f3a4b92942c6a61742553252d46
trodevel <trodevel@gmail.com>
2020-06-15 04:22:22 +0200

    added using

--- dummy_creator.cpp
+++ dummy_creator.cpp
@@ -19,7 +19,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-// $Revision: 13268 $ $Date:: 2020-06-15 #$ $Author: serge $
+// $Revision: 13272 $ $Date:: 2020-06-15 #$ $Author: serge $
 
 #include "dummy_creator.h"             // self
 
@@ -34,13 +34,14 @@ namespace dummy
 using ::basic_parser::dummy::create__uint8;
 using ::basic_parser::dummy::create__uint32;
 using ::basic_parser::dummy::create__bool;
+using ::basic_parser::dummy::create__string;
 
 TimePoint24 create__TimePoint24()
 {
     TimePoint24 res;
 
-    res.hh = ::basic_parser::dummy::create__uint8() % 24;
-    res.mm = ::basic_parser::dummy::create__uint8() % 60;
+    res.hh = create__uint8() % 24;
+    res.mm = create__uint8() % 60;
 
     return res;
 }
@@ -59,12 +60,12 @@ LocalTime create__LocalTime()
 {
     LocalTime res;
 
-    res.year   = ::basic_parser::dummy::create__uint8() % 24 + 2010;
-    res.month  = ::basic_parser::dummy::create__uint8() % 12;
-    res.day    = ::basic_parser::dummy::create__uint8() % 30;
-    res.hh = ::basic_parser::dummy::create__uint8() % 24;
-    res.mm = ::basic_parser::dummy::create__uint8() % 60;
-    res.ss = ::basic_parser::dummy::create__uint8() % 60;
+    res.year   = create__uint8() % 24 + 2010;
+    res.month  = create__uint8() % 12;
+    res.day    = create__uint8() % 30;
+    res.hh = create__uint8() % 24;
+    res.mm = create__uint8() % 60;
+    res.ss = create__uint8() % 60;
 
     return res;
 }
@@ -74,13 +75,13 @@ Weekdays create__Weekdays()
     Weekdays res;
 
     res.mask   =
-            Weekdays::weekdays_e::MO * ::basic_parser::dummy::create__bool()+
-            Weekdays::weekdays_e::TU * ::basic_parser::dummy::create__bool() +
-            Weekdays::weekdays_e::WE * ::basic_parser::dummy::create__bool() +
-            Weekdays::weekdays_e::TH * ::basic_parser::dummy::create__bool() +
-            Weekdays::weekdays_e::FR * ::basic_parser::dummy::create__bool() +
-            Weekdays::weekdays_e::SA * ::basic_parser::dummy::create__bool() +
-            Weekdays::weekdays_e::SU * ::basic_parser::dummy::create__bool();
+            Weekdays::weekdays_e::MO * create__bool() +
+            Weekdays::weekdays_e::TU * create__bool() +
+            Weekdays::weekdays_e::WE * create__bool() +
+            Weekdays::weekdays_e::TH * create__bool() +
+            Weekdays::weekdays_e::FR * create__bool() +
+            Weekdays::weekdays_e::SA * create__bool() +
+            Weekdays::weekdays_e::SU * create__bool();
 
     return res;
 }
@@ -89,8 +90,8 @@ TimeRange create__TimeRange()
 {
     TimeRange res;
 
-    res.from = ::basic_parser::dummy::create__uint32() + 1500000000;
-    res.to = ::basic_parser::dummy::create__uint32() + 1500000000;
+    res.from = create__uint32() + 1500000000;
+    res.to = create__uint32() + 1500000000;
 
     return res;
 }
@@ -109,9 +110,9 @@ Date create__Date()
 {
     Date res;
 
-    res.year   = ::basic_parser::dummy::create__uint8() % 24 + 2010;
-    res.month  = ::basic_parser::dummy::create__uint8() % 12;
-    res.day    = ::basic_parser::dummy::create__uint8() % 30;
+    res.year   = create__uint8() % 24 + 2010;
+    res.month  = create__uint8() % 12;
+    res.day    = create__uint8() % 30;
 
     return res;
 }
@@ -120,10 +121,7 @@ Email create__Email()
 {
     Email res;
 
-    res.email = ::basic_parser::dummy::create__string() +
-        "@" +
-        ::basic_parser::dummy::create__string() + 
-        ".com";
+    res.email = create__string() + "@" + create__string() + ".com";
 
     return res;
 }
