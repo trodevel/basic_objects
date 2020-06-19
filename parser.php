2c49449638a069cfbd7e9e1ad04fee168c3b25b3
trodevel <trodevel@gmail.com>
2020-06-19 03:10:01 +0200

    regenerated

--- parser.php
+++ parser.php
@@ -8,85 +8,85 @@ require_once __DIR__.'/../basic_parser/parser.php';
 
 // objects
 
-function parse_TimePoint24( & $csv_arr, & $offset )
+function parse__TimePoint24( & $csv_arr, & $offset )
 {
     $res = new TimePoint24;
 
-    $res->hh = \basic_parser\parse_int( $csv_arr, $offset );
-    $res->mm = \basic_parser\parse_int( $csv_arr, $offset );
+    $res->hh = \basic_parser\parse__int( $csv_arr, $offset );
+    $res->mm = \basic_parser\parse__int( $csv_arr, $offset );
 
     return $res;
 }
 
-function parse_TimeWindow( & $csv_arr, & $offset )
+function parse__TimeWindow( & $csv_arr, & $offset )
 {
     $res = new TimeWindow;
 
-    $res->from = parse_TimePoint24( $csv_arr, $offset );
-    $res->to = parse_TimePoint24( $csv_arr, $offset );
+    $res->from = parse__TimePoint24( $csv_arr, $offset );
+    $res->to = parse__TimePoint24( $csv_arr, $offset );
 
     return $res;
 }
 
-function parse_LocalTime( & $csv_arr, & $offset )
+function parse__LocalTime( & $csv_arr, & $offset )
 {
     $res = new LocalTime;
 
-    $res->year = \basic_parser\parse_int( $csv_arr, $offset );
-    $res->month = \basic_parser\parse_int( $csv_arr, $offset );
-    $res->day = \basic_parser\parse_int( $csv_arr, $offset );
-    $res->hh = \basic_parser\parse_int( $csv_arr, $offset );
-    $res->mm = \basic_parser\parse_int( $csv_arr, $offset );
-    $res->ss = \basic_parser\parse_int( $csv_arr, $offset );
+    $res->year = \basic_parser\parse__int( $csv_arr, $offset );
+    $res->month = \basic_parser\parse__int( $csv_arr, $offset );
+    $res->day = \basic_parser\parse__int( $csv_arr, $offset );
+    $res->hh = \basic_parser\parse__int( $csv_arr, $offset );
+    $res->mm = \basic_parser\parse__int( $csv_arr, $offset );
+    $res->ss = \basic_parser\parse__int( $csv_arr, $offset );
 
     return $res;
 }
 
-function parse_Weekdays( & $csv_arr, & $offset )
+function parse__Weekdays( & $csv_arr, & $offset )
 {
     $res = new Weekdays;
 
-    $res->mask = \basic_parser\parse_int( $csv_arr, $offset );
+    $res->mask = \basic_parser\parse__int( $csv_arr, $offset );
 
     return $res;
 }
 
-function parse_TimeRange( & $csv_arr, & $offset )
+function parse__TimeRange( & $csv_arr, & $offset )
 {
     $res = new TimeRange;
 
-    $res->from = \basic_parser\parse_int( $csv_arr, $offset );
-    $res->to = \basic_parser\parse_int( $csv_arr, $offset );
+    $res->from = \basic_parser\parse__int( $csv_arr, $offset );
+    $res->to = \basic_parser\parse__int( $csv_arr, $offset );
 
     return $res;
 }
 
-function parse_LocalTimeRange( & $csv_arr, & $offset )
+function parse__LocalTimeRange( & $csv_arr, & $offset )
 {
     $res = new LocalTimeRange;
 
-    $res->from = parse_LocalTime( $csv_arr, $offset );
-    $res->to = parse_LocalTime( $csv_arr, $offset );
+    $res->from = parse__LocalTime( $csv_arr, $offset );
+    $res->to = parse__LocalTime( $csv_arr, $offset );
 
     return $res;
 }
 
-function parse_Date( & $csv_arr, & $offset )
+function parse__Date( & $csv_arr, & $offset )
 {
     $res = new Date;
 
-    $res->year = \basic_parser\parse_int( $csv_arr, $offset );
-    $res->month = \basic_parser\parse_int( $csv_arr, $offset );
-    $res->day = \basic_parser\parse_int( $csv_arr, $offset );
+    $res->year = \basic_parser\parse__int( $csv_arr, $offset );
+    $res->month = \basic_parser\parse__int( $csv_arr, $offset );
+    $res->day = \basic_parser\parse__int( $csv_arr, $offset );
 
     return $res;
 }
 
-function parse_Email( & $csv_arr, & $offset )
+function parse__Email( & $csv_arr, & $offset )
 {
     $res = new Email;
 
-    $res->email = \basic_parser\parse_string( $csv_arr, $offset );
+    $res->email = \basic_parser\parse__string( $csv_arr, $offset );
 
     return $res;
 }
