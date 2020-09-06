1d1f7de4cff3be9da4f33aafaa063c898aca7bf9
trodevel <trodevel@gmail.com>
2020-09-06 03:49:53 +0200

    regenerated

--- object_initializer.php
+++ object_initializer.php
@@ -3,6 +3,9 @@
 namespace basic_objects;
 
 
+// includes
+require_once 'protocol.php';
+
 // objects
 
 function initialize__TimePoint24( & $res
@@ -86,6 +89,107 @@ function initialize__Email( & $res
 // base messages
 
 // messages
+
+// objects (constructors)
+
+function create__TimePoint24(
+    $hh // uint8_t
+    , $mm // uint8_t
+ )
+{
+    $res = new TimePoint24;
+
+    initialize__TimePoint24( $res, $hh, $mm );
+
+    return $res;
+}
+
+function create__TimeWindow(
+    $from // TimePoint24
+    , $to // TimePoint24
+ )
+{
+    $res = new TimeWindow;
+
+    initialize__TimeWindow( $res, $from, $to );
+
+    return $res;
+}
+
+function create__LocalTime(
+    $year // uint32_t
+    , $month // uint8_t
+    , $day // uint8_t
+    , $hh // uint8_t
+    , $mm // uint8_t
+    , $ss // uint8_t
+ )
+{
+    $res = new LocalTime;
+
+    initialize__LocalTime( $res, $year, $month, $day, $hh, $mm, $ss );
+
+    return $res;
+}
+
+function create__Weekdays(
+    $mask // uint32_t
+ )
+{
+    $res = new Weekdays;
+
+    initialize__Weekdays( $res, $mask );
+
+    return $res;
+}
+
+function create__TimeRange(
+    $from // uint32_t
+    , $to // uint32_t
+ )
+{
+    $res = new TimeRange;
+
+    initialize__TimeRange( $res, $from, $to );
+
+    return $res;
+}
+
+function create__LocalTimeRange(
+    $from // LocalTime
+    , $to // LocalTime
+ )
+{
+    $res = new LocalTimeRange;
+
+    initialize__LocalTimeRange( $res, $from, $to );
+
+    return $res;
+}
+
+function create__Date(
+    $year // uint32_t
+    , $month // uint8_t
+    , $day // uint8_t
+ )
+{
+    $res = new Date;
+
+    initialize__Date( $res, $year, $month, $day );
+
+    return $res;
+}
+
+function create__Email(
+    $email // string
+ )
+{
+    $res = new Email;
+
+    initialize__Email( $res, $email );
+
+    return $res;
+}
 
 // messages (constructors)
 
